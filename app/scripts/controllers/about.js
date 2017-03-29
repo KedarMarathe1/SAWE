f5b3ca3e7e1fc76084a43e990864f4ab5565e8a9
KedarMarathe1 <kedar.the1@gmail.com>
2017-03-29 16:15:17 +0530

    tentative UI

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saweApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the saweApp
+ */
+angular.module('saweApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saweApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the saweApp
+ */
+angular.module('saweApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
