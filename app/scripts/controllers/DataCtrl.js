57982407f26a3f3d5eafb0afc1cd251af45004e4
KedarMarathe1 <kedar.the1@gmail.com>
2017-04-06 12:22:47 +0530

    removed controllers

--- app/scripts/controllers/DataCtrl.js
+++ app/scripts/controllers/DataCtrl.js
@@ -4,22 +4,22 @@
 angular.module('saweApp',[])
   .controller('DataCtrl',['$scope','$http', function ($scope,$http) {
 
-      $http.get('../../Data/Projects.json').then(function(result){
+      $http.get('Data/Projects.json').then(function(result){
           $scope.ProjectRows =$scope.getRows(result.data,3);
            
 
       });
 
-         $http.get('../../Data/AboutUs.json').then(function(result){
+         $http.get('Data/AboutUs.json').then(function(result){
           $scope.AboutUsRows= $scope.getRows(result.data,3);
           console.log($scope.ProjectRows);
       });
 
-        $http.get('../../Data/Achievements.json').then(function(result){
+        $http.get('Data/Achievements.json').then(function(result){
           $scope.Achievements = result.data;
       });
 
-        $http.get('../../Data/Team.json').then(function(result){
+        $http.get('Data/Team.json').then(function(result){
           $scope.TeamData = result.data;
           $scope.Team = $scope.getRows(result.data.team,4);
           $scope.helpingHands = $scope.getRows(result.data.helpingHands,6)
